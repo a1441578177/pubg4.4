f0cff4a511cddedc9470d802ef1c0615b6a52522
a1441578177 <37325492+a1441578177@users.noreply.github.com>
2018-05-18 00:34:21 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+cp /root/pubg4.4/restart.sh /root/restart.sh
+chmod +x restart.sh
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
+123456789
+8080
