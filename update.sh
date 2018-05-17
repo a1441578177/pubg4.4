4e59d483a33d3b873107394003650c5dce9f200a
a1441578177 <37325492+a1441578177@users.noreply.github.com>
2018-05-18 00:37:56 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,3 @@ chmod +x restart.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
-123456789
-8080
